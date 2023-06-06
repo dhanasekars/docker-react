1c7837e0aeadd9e10de60506ec774f6ebf626ce4
DS <dhanasekars@gmail.com>
2023-06-06 06:42:23 +0530

    home page content updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        This is the new world order updated by local feature.
+        This is the new world order updated by feature branch.
         </p>
         <a
           className="App-link"
